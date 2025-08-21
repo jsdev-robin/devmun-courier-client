54ef4629d52b97cc368276743c039c7513ebf4a4
Md Robin <jsdev.robin@gmail.com>
2025-08-21 13:49:13 +0600

    Add User Info

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="h-svh flex items-center justify-center flex-col gap-4 fixed top-0 left-0 bottom-0 right-0 bg-background z-50">
+      Loading...
+    </div>
+  );
+};
+
+export default Loading;
