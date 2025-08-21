b738156cff866f7821f344cdf36f159016c7b40a
Md Robin <jsdev.robin@gmail.com>
2025-08-21 12:52:58 +0600

    Commit this CustomerBookParcel

--- src/app/(web)/your/dashboard/page.tsx
+++ src/app/(web)/your/dashboard/page.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-// import CustomerAgentTrackingMap from '../../../../components/dashboard/customer/CustomerAgentTrackingMap';
-import CustomerBookParcel from '../../../../components/dashboard/customer/CustomerBookParcel';
+import CustomerAgentTrackingMap from '../../../../components/dashboard/customer/CustomerAgentTrackingMap';
+// import CustomerBookParcel from '../../../../components/dashboard/customer/CustomerBookParcel';
 import CustomerTrackParcel from '../../../../components/dashboard/customer/CustomerTrackParcel';
 import CustomerParcels from '../../../../components/dashboard/customer/CustomerParcels';
 
 const page = () => {
   return (
     <>
-      {/* <CustomerAgentTrackingMap /> */}
-      <CustomerBookParcel />
+      <CustomerAgentTrackingMap />
+      {/* <CustomerBookParcel /> */}
       <CustomerTrackParcel />
       <CustomerParcels />
     </>
